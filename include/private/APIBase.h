d0a8e9aeeb8961a72f0fdf192266992177c91666
R. Keith Morley <keithroe@gmail.com>
2011-06-07 18:11:23 -0600

    Added name field to APIBase

--- include/private/APIBase.h
+++ include/private/APIBase.h
@@ -2,8 +2,19 @@
 #ifndef LEGION_APIBASE_H_
 #define LEGION_APIBASE_H_
 
+#include <memory>
+
 class APIBase
 {
+public:
+    APIBase( const std::string& name );
+    virtual ~APIBase();
+
+    const std::string getName();
+
+private:
+    class Impl;
+    std::shared_ptr<Impl> m_impl;
 };
 
 #endif // LEGION_APIBASE_H_
