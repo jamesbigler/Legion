e4f3f9d6e05e0499c20453585c6ccb883b64d132
keith <keithroe@gmail.com>
2013-02-04 09:12:16 -0700

    Switching machines

--- src/Legion/Objects/Surface/ISurface.hpp
+++ src/Legion/Objects/Surface/ISurface.hpp
@@ -1,9 +1,29 @@
 
+// Copyright (C) 2011 R. Keith Morley 
+// 
+// (MIT/X11 License)
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
 
 /// \file ISurface.hpp
 /// Pure virtual interface for Surface Shaders
-#ifndef LEGION_OBJECTS_SURFACESHADER_ISURFACESHADER_H_
-#define LEGION_OBJECTS_SURFACESHADER_ISURFACESHADER_H_
+#ifndef LEGION_OBJECTS_SURFACE_ISURFACE_HPP_
+#define LEGION_OBJECTS_SURFACE_ISURFACE_HPP_
 
 #include <Legion/Objects/IObject.hpp>
 
@@ -78,4 +98,4 @@ public:
 
 }
 
-#endif // LEGION_OBJECTS_SURFACESHADER_ISURFACESHADER_H_
+#endif // LEGION_OBJECTS_SURFACE_ISURFACE_HPP_
